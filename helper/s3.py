09ffda9c8b6c9805cd797f1a6aaf02e8e4366d74
Tyler Fisher <tyler@tylerfisher.org>
2016-01-07 22:04:38 -0500

    Add prefix to ooni-probe report names

--- helper/s3.py
+++ helper/s3.py
@@ -1,7 +1,13 @@
 from pprint import pprint
+import pandas as pd
 import luigi.s3
 import constants
 import logging
+import boto
+import os
+
+boto.set_stream_logger('boto')
+logging.getLogger('boto').setLevel(logging.INFO)
 
 
 def connect():
@@ -13,11 +19,15 @@ def connect():
 
 def get_keys(connection, prefix, has_any=None, has_all=None, date_prefix=None):
     logging.info("Getting keys from S3 with prefix: %s" % prefix)
-    keys = set(connection.list(prefix))
+    keys = set(map(lambda k: os.path.join(prefix, k), connection.list(prefix)))
     if date_prefix:
-        keys = set(filter(lambda k: k.startswith(date_prefix), keys))
+        keys = set(filter(lambda k: any(map(lambda d: d in k, date_prefix)), keys))
     if has_any:
         keys = set(filter(lambda k: any(map(lambda t: t in k, has_any)), keys))
     if has_all:
         keys = set(filter(lambda k: all(map(lambda t: t in k, has_all)), keys))
     return keys
+
+
+def wrap_as_s3_target(connection, keys):
+    return list(map(lambda e: luigi.s3.S3Target(e, client=connection), keys))
--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 from pprint import pprint
-import itertools
-import functools
 import luigi.s3
 import luigi
 import logging
-import boto
 
 import helper.s3
 import constants
@@ -21,36 +18,29 @@ class FetchOoniProbeReports(luigi.ExternalTask):
     Fetches raw, unsanitised ooni-probe reports from AWS S3
     """
     def output(self):
-        return self.get_raw_report_keys_from_s3()
+        connection = luigi.s3.S3Client(
+                aws_access_key_id=constants.credentials['aws_access_key_id'],
+                aws_secret_access_key=constants.credentials['aws_secret_access_key'],
+        )
+        dates = tuple(pd.date_range('2016-01-01', '2016-01-05').strftime('%Y-%m-%d'))
+        keys = helper.s3.get_keys(
+                connection=connection,
+                prefix=constants.ooni_s3_targets['raw'],
+                date_prefix=dates,
+                has_any=['traceroute'])
+        return helper.s3.wrap_as_s3_target(connection=connection, keys=keys)
 
 
 class OoniProbeReportsToJson(luigi.Task):
     def requires(self):
-        pass
+        return FetchOoniProbeReports()
 
     def run(self):
-        pass
+        print("Hello")
 
     def output(self):
         pass
 
 
-def get_raw_report_keys_from_s3():
-    connection = luigi.s3.S3Client(
-            aws_access_key_id=constants.credentials['aws_access_key_id'],
-            aws_secret_access_key=constants.credentials['aws_secret_access_key'],
-    )
-    dates = tuple(pd.date_range('2016-01-01', '2016-01-05').strftime('%Y-%m-%d'))
-    targets = set()
-    keys = helper.s3.get_keys(
-            connection=connection,
-            prefix=constants.ooni_s3_targets['raw'],
-            date_prefix=dates,
-            has_any=['traceroute'])
-    return keys
-
 if __name__ == '__main__':
-    keys = get_raw_report_keys_from_s3()
-    pprint(keys)
-
-    #luigi.run()
+    luigi.run()
